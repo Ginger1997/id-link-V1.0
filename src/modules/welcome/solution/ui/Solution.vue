c3e6f5c7df92cd7fdc30c4f72a4acc86aab4fdfe
Michael Alejandro <michael.alejandros@ug.edu.ec>
2021-06-29 00:55:15 -0500

    cambios

--- src/modules/welcome/solution/ui/Solution.vue
+++ src/modules/welcome/solution/ui/Solution.vue
@@ -18,10 +18,19 @@
     <!-- Tab panes -->
     <div class="tab-content">
       <div id="menu1" class="container tab-pane active"><br>
-        <h3>Cloud Data Hosting</h3>
-        <ul>
-          <li><p>Keep your data closer and safer wherever you are and whenever you need it.</p></li>
-        </ul>
+        <div class="row">
+          <h3>Cloud Data Hosting</h3>
+          <div class="col-10">
+            <ul>
+              <li><p>Keep your data closer and safer wherever you are and whenever you need it.</p></li>
+            </ul>
+          </div>
+          <div class="col-2">
+            <img class="rounded " src="@/assets/pictures/Cloud.png" style=" filter: invert(0.4);width: 200px" alt="">
+          </div>
+        </div>
+
+
 
       </div>
       <div id="menu2" class="container tab-pane fade"><br>
@@ -30,6 +39,7 @@
           <li><p>Have access to one or multiple projects instantly in any device.</p></li>
           <li><p>Virtual RTOC concept.</p></li>
         </ul>
+        <img src="@/assets/pictures/Cloud.png" style=" filter: invert(0.4);" alt="">
       </div>
       <div id="menu3" class="container tab-pane fade"><br>
         <h3>Well Management Platform</h3>
@@ -38,6 +48,9 @@
           <li><p>Give access to your collaborators, partners, clients or key personnel you trust.</p></li>
           <li><p>Manage all your all your projects and  your data in one place.</p></li>
         </ul>
+        <span class="float-right">
+          <img src="@/assets/pictures/Cloud.png" style=" filter: invert(0.4);" alt="">
+        </span>
 
       </div>
     </div>
